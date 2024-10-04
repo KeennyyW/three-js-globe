7586c4ec11be7457362dbdd1bf6546a5a47d2b16
Laurin04 <laurinlattanzi12@gmail.com>
2024-10-04 15:05:05 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -14,7 +12,7 @@ function App() {
         </h1>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        
       </p>
     </>
   )
