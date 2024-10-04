82b44fa6a3d5083661b9c15376d18f86c5ec1001
Laurin04 <laurinlattanzi12@gmail.com>
2024-10-04 19:38:11 +0200

    globe

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,102 @@
-import './App.css';
-import * as THREE from 'three';
-import { useEffect } from 'react';
-import './App.css';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// import ThreeGlobe from 'three-globe';
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+import ThreeGlobe from "three-globe";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { createGlowMesh } from "three-glow-mesh";
+import countries from "./files/globe-data-min.json";
+import travelHistory from "./files/my-flights.json";
+import airportHistory from "./files/my-airports.json";
 
+const App: React.FC = () => {
+  const mountRef = useRef<HTMLDivElement>(null);
 
-function App() {
   useEffect(() => {
-    const scene = new THREE.Scene();
+    let renderer, camera, scene, controls;
+    let mouseX = 0;
+    let mouseY = 0;
+    let windowHalfX = window.innerWidth / 2;
+    let windowHalfY = window.innerHeight / 2;
+    let Globe: any;
 
-    const camera = new THREE.PerspectiveCamera(
-      50, 
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.z = 96;
-
-    const canvas = document.getElementById('myThreeJsCanvas');
-    const renderer = new THREE.WebGLRenderer({
-      canvas, 
-      antialias: true,
-    });
-    
+    // Initialize renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    if (mountRef.current) {
+      mountRef.current.appendChild(renderer.domElement);
+    }
+
+    // Initialize scene, light
+    scene = new THREE.Scene();
+    scene.add(new THREE.AmbientLight(0xbbbbbb, 0.3));
+    scene.background = new THREE.Color(0x040d21);
+
+    // Initialize camera, light
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 400;
+    camera.position.x = 0;
+    camera.position.y = 0;
+
+    const dLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    dLight.position.set(-800, 2000, 400);
+    camera.add(dLight);
+
+    const dLight1 = new THREE.DirectionalLight(0x7982f6, 1);
+    dLight1.position.set(-200, 500, 200);
+    camera.add(dLight1);
+
+    const dLight2 = new THREE.PointLight(0x8566cc, 0.5);
+    dLight2.position.set(-200, 500, 200);
+    camera.add(dLight2);
+
+    scene.add(camera);
+
+    // Additional effects
+    scene.fog = new THREE.Fog(0x535ef3, 400, 2000);
+
+    // Initialize controls
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dynamicDampingFactor = 0.01;
+    controls.enablePan = false;
+    controls.minDistance = 200;
+    controls.maxDistance = 500;
+    controls.rotateSpeed = 0.8;
+    controls.zoomSpeed = 1;
+    controls.autoRotate = false;
+    controls.minPolarAngle = Math.PI / 3.5;
+    controls.maxPolarAngle = Math.PI - Math.PI / 3;
+
+    // Initialize Globe
+    Globe = new ThreeGlobe({
+      waitForGlobeReady: true,
+      animateIn: true,
+    })
+      .hexPolygonsData(countries.features)
+      .hexPolygonResolution(3)
+      .hexPolygonMargin(0.7)
+      .showAtmosphere(true)
+      .atmosphereColor("#3a228a")
+      .atmosphereAltitude(0.25)
+      .hexPolygonColor((e) => {
+        if (
+          ["KGZ", "KOR", "THA", "RUS", "UZB", "IDN", "KAZ", "MYS"].includes(
+            e.properties.ISO_A3
+          )
+        ) {
+          return "rgba(255,255,255, 1)";
+        } else return "rgba(255,255,255, 0.7)";
+      });
+
+    Globe.rotateY(-Math.PI * (5 / 9));
+    Globe.rotateZ(-Math.PI / 6);
+    const globeMaterial = Globe.globeMaterial();
+    globeMaterial.color = new THREE.Color(0x3a228a);
+    globeMaterial.emissive = new THREE.Color(0x220038);
+    globeMaterial.emissiveIntensity = 0.1;
+    globeMaterial.shininess = 0.7;
+
+    scene.add(Globe);
 
     // Handle window resize
     const onWindowResize = () => {
@@ -34,78 +105,35 @@ function App() {
       renderer.setSize(window.innerWidth, window.innerHeight);
     };
 
-    window.addEventListener('resize', onWindowResize);
+    window.addEventListener("resize", onWindowResize, false);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
+    const onMouseMove = (event: MouseEvent) => {
+      mouseX = event.clientX - windowHalfX;
+      mouseY = event.clientY - windowHalfY;
+    };
 
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
-
-    var dLight = new THREE.DirectionalLight(0xffffff, 0.8);
-    dLight.position.set(-800, 2000, 400);
-    camera.add(dLight);
-
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dynamicDampingFactor = 0.01;
-    controls.enablePan = false;
-    controls.minDistance = 10;
-    controls.maxDistance = 500;
-    controls.rotateSpeed = 0.8;
-    controls.zoomSpeed = 1;
-    controls.autoRotate = false;
-
-    controls.minPolarAngle = Math.PI / 3.5;
-    controls.maxPolarAngle = Math.PI - Math.PI / 3;
-
-  
-
-    // function initGlobe() {
-    //   const Globe = new ThreeGlobe();
-
-    //   const globeMaterial = Globe.globeMaterial();
-    //   globeMaterial.color = new THREE.Color(0x3a228a);
-    //   globeMaterial.emissive = new THREE.Color(0x220038);
-    //   globeMaterial.emissiveIntesity = 0.1;
-    //   globeMaterial.shininess = 0.7;
-      
-    //   scene.add(boxMesh);
-    // }
-
-    scene.background = new THREE.Color(0x040d21);
-
-    scene.add(boxMesh);
+    document.addEventListener("mousemove", onMouseMove);
 
     const animate = () => {
-      boxMesh.rotation.x += 0.006;
-      boxMesh.rotation.y += 0.006;
-
+      camera.lookAt(scene.position);
+      controls.update();
       renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
+      Globe.rotation.y += 0.002;
+      requestAnimationFrame(animate);
     };
+
     animate();
 
-    // Clean up event listener on component unmount
     return () => {
-      window.removeEventListener('resize', onWindowResize);
+      window.removeEventListener("resize", onWindowResize);
+      document.removeEventListener("mousemove", onMouseMove);
+      if (mountRef.current) {
+        mountRef.current.removeChild(renderer.domElement);
+      }
     };
   }, []);
 
-  return (
-    <>
-      <div className='App'>
-        <canvas className='canvasCSS' id='myThreeJsCanvas'></canvas>
-      </div>
-    </>
-  );
-}
+  return <div ref={mountRef} style={{ width: "100vw", height: "100vh" }} />;
+};
 
 export default App;
