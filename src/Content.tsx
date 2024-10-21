fd30dffb8028fb3da9beebc22519a7605d09215f
Laurin04 <laurinlattanzi12@gmail.com>
2024-10-21 11:50:48 +0200

    update

--- src/Content.tsx
+++ src/Content.tsx
@@ -7,24 +7,39 @@ import {useState} from "react";
 
 
 const MyComponent = () => {
-    const [hidden, setHidden] = useState(true)
+    const [isHoveredBox1, setHoveredBox1] = useState(false);
+    const [isHoveredBox2, setHoveredBox2] = useState(false);
 
+    return (
+        <div className="Content">
+            <div className="main-content">
+                <div className="fact-box"
+                     onMouseEnter={() => setHoveredBox1(true)}
+                     onMouseLeave={() => setHoveredBox1(false)}>
+                    <p>Der Datenverkehr trägt 33% zu den globalen CO2 Emissionen bei</p>
+                    <div>
+                        {isHoveredBox1 ? (
+                            <div>
+                                <img src="src/files/Screenshot 2024-10-21 111032.png" alt="" className="first-image"/>
+                            </div>
+                        ) : null}
+                    </div>
+                </div>
 
-  return (
-      <div className="Content">
-          <div className="main-content">
-              <div className="fact-box"
-              onMouseEnter={() => setHidden(false)}
-              onMouseLeave={() => setHidden(true)}>
-                  <p>Der Datenverkehr trägt 33% zu den globalen CO2 Emissionen bei</p>
-                  <div>
-                      {hidden ? null : <h1>Hover</h1>}
-                  </div>
+                <div className="fact-box"
+                     onMouseEnter={() => setHoveredBox2(true)}
+                     onMouseLeave={() => setHoveredBox2(false)}>
+                    <p>Der Speicher von Google betrug 1 Exabyte im Jahre 2010, heutzutage beträgt alleine der jährliche
+                        Zuwachs des Speichers 6.2 Exabyte.</p>
+                    <div>
+                        {isHoveredBox2 ? (
+                            <div>
+                                <img src="src/files/image2.png" alt="" className="first-image"/>
+                            </div>
+                        ) : null}
+                    </div>
+                </div>
 
-              </div>
-              <div className="fact-box">
-                  <p>Der Speicher von Google betrug 1 Exabyte im Jahre 2010, heutzutage beträgt alleine der jährliche Zuwachs des Speichers 6.2 Exabyte.</p>
-              </div>
               <div className="fact-box">
                   <p>Alle 8000 Rechenzentren verbrauchen etwa 416 Terrawattstunden im Jahr. Ganz Österreich verbraucht vergleichsweise 12 Terrawattstunden im Jahr. </p>
               </div>
